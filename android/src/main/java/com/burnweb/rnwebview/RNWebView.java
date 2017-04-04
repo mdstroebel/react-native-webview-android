72dd72e10ed5a68c6a2d01ed547ca06ba5e522db
Marc Stroebel <marc.stroebel@24.com>
2017-04-04 09:00:57 +0200

    Added support for full-screen video play for the Android web view. Credit to T-Spoon on GitHub (https://github.com/T-Spoon/react-native/tree/master/ReactAndroid/src/main/java/com/facebook/react/views/webview).

--- android/src/main/java/com/burnweb/rnwebview/RNWebView.java
+++ android/src/main/java/com/burnweb/rnwebview/RNWebView.java
@@ -73,6 +73,60 @@ class RNWebView extends WebView implements LifecycleEventListener {
         public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
             return getModule().startFileChooserIntent(filePathCallback, fileChooserParams.createIntent());
         }
+
+        // Support for full-screen video
+        public CustomWebChromeClient(Activity activity, WebView webView) {
+            mWebView = webView;
+            mActivity = activity;
+        }
+
+        private final FrameLayout.LayoutParams FULLSCREEN_LAYOUT_PARAMS = new FrameLayout.LayoutParams(
+                LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, Gravity.CENTER);
+
+        private WebChromeClient.CustomViewCallback mCustomViewCallback;
+
+        private Activity mActivity;
+        private View mWebView;
+        private View mVideoView;
+
+        @Override
+        public void onShowCustomView(View view, CustomViewCallback callback) {
+            if (mVideoView != null) {
+                callback.onCustomViewHidden();
+                return;
+            }
+
+            // Store the view and it's callback for later, so we can dispose of them correctly
+            mVideoView = view;
+            mCustomViewCallback = callback;
+
+            view.setBackgroundColor(Color.BLACK);
+
+            getRootView().addView(view, FULLSCREEN_LAYOUT_PARAMS);
+
+            mWebView.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onHideCustomView() {
+            if (mVideoView == null) {
+                return;
+            }
+
+            mVideoView.setVisibility(View.GONE);
+
+            // Remove the custom view from its container.
+            getRootView().removeView(mVideoView);
+            mVideoView = null;
+            mCustomViewCallback.onCustomViewHidden();
+
+            mWebView.setVisibility(View.VISIBLE);
+        }
+
+        private ViewGroup getRootView() {
+            return ((ViewGroup) mActivity.findViewById(android.R.id.content));
+        }
+
     }
 
     protected class GeoWebChromeClient extends CustomWebChromeClient {
